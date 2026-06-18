9a026b4550d158be8456a4489eb82c5223484c6d
Abdullah Siddiqui <abdullahsidd37@gmail.com>
2026-06-18 17:38:42 +0500

    g

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -11,9 +11,9 @@ const Navbar = () => {
   const [show, setShow] = useState(false);
   // const { isAuthorized, setIsAuthorized, user } = useContext(Context);
 
-    const { isAuthorized, user } = useSelector((state) => state.auth)
-    console.log("user",user);
-    
+  const { isAuthorized, user } = useSelector((state) => state.auth)
+  console.log("user", user);
+
 
   const navigateTo = useNavigate();
 
@@ -36,128 +36,127 @@ const Navbar = () => {
   // ${isAuthorized ? "block" : "hidden"}
 
   return (
-<nav className={` bg-[#18191c] px-3`}>
-  <div className="max-w-[1500px] mx-auto flex justify-between items-center">
-    
-    {/* Logo */}
-    <div className="w-[100px] h-[80px]">
-      <img
-        src={employeeLogo}
-        alt="Logo"
-        className="w-full h-full object-contain"
-      />
-    </div>
+    <nav className={` bg-[#18191c] px-3`}>
+      <div className="max-w-[1500px] mx-auto flex justify-between items-center">
 
-    {/* Menu */}
-    <ul
-      className={`${
-        show
-          ? "fixed left-0 top-[120px] flex flex-col justify-center items-start gap-8 pl-6 w-[400px] h-screen bg-[#f1f3f6] shadow-lg lg:static lg:flex-row lg:w-auto lg:h-auto lg:bg-transparent lg:shadow-none"
-          : "hidden lg:flex"
-      } items-center gap-6`}
-    >
+        {/* Logo */}
+        <div className="w-[100px] h-[80px]">
+          <img
+            src={employeeLogo}
+            alt="Logo"
+            className="w-full h-full object-contain"
+          />
+        </div>
 
-      {
-        user.role == "Employer" ? (
-          <>
-          <li>
-        <Link
-          to="/"
-          onClick={() => setShow(false)}
-          className="text-[#184235] lg:text-[#f1f3f6] text-md font-semibold lg:font-light hover:text-[#2d5649] transition"
+        {/* Menu */}
+        <ul
+          className={`${show
+            ? "fixed left-0 top-[120px] flex flex-col justify-center items-start gap-8 pl-6 w-[400px] h-screen bg-[#f1f3f6] shadow-lg lg:static lg:flex-row lg:w-auto lg:h-auto lg:bg-transparent lg:shadow-none"
+            : "hidden lg:flex"
+            } items-center gap-6`}
         >
-          HOME
-        </Link>
-      </li>
 
-      <li>
-        <Link
-          to="/job/getall"
-          onClick={() => setShow(false)}
-          className="text-[#184235] lg:text-[#f1f3f6] text-md font-semibold lg:font-light hover:text-[#2d5649] transition"
-        >
-          ALL JOBS
-        </Link>
-      </li>
+          {
+            user?.role == "Employer" ? (
+              <>
+                <li>
+                  <Link
+                    to="/"
+                    onClick={() => setShow(false)}
+                    className="text-[#184235] lg:text-[#f1f3f6] text-md font-semibold lg:font-light hover:text-[#2d5649] transition"
+                  >
+                    HOME
+                  </Link>
+                </li>
 
-
-      <li>
-        <Link
-          to="/job/getall"
-          onClick={() => setShow(false)}
-          className="text-[#184235] lg:text-[#f1f3f6] text-md font-semibold lg:font-light hover:text-[#2d5649] transition"
-        >
-          POST JOBS
-        </Link>
-      </li>
-
-      <li>
-        <Link
-          to="/job/getall"
-          onClick={() => setShow(false)}
-          className="text-[#184235] lg:text-[#f1f3f6] text-md font-semibold lg:font-light hover:text-[#2d5649] transition"
-        >
-          MY JOBS
-        </Link>
-      </li>
+                <li>
+                  <Link
+                    to="/job/getall"
+                    onClick={() => setShow(false)}
+                    className="text-[#184235] lg:text-[#f1f3f6] text-md font-semibold lg:font-light hover:text-[#2d5649] transition"
+                  >
+                    ALL JOBS
+                  </Link>
+                </li>
 
 
-      <li>
-        <Link
-          to="/applications/me"
-          onClick={() => setShow(false)}
-          className="text-[#184235] lg:text-[#f1f3f6] text-md font-semibold lg:font-light hover:text-[#2d5649] transition"
-        >
-           "APPLICANTS"
-        </Link>
-      </li>
-          </>
+                <li>
+                  <Link
+                    to="/job/getall"
+                    onClick={() => setShow(false)}
+                    className="text-[#184235] lg:text-[#f1f3f6] text-md font-semibold lg:font-light hover:text-[#2d5649] transition"
+                  >
+                    POST JOBS
+                  </Link>
+                </li>
 
-        ) : (
+                <li>
+                  <Link
+                    to="/job/getall"
+                    onClick={() => setShow(false)}
+                    className="text-[#184235] lg:text-[#f1f3f6] text-md font-semibold lg:font-light hover:text-[#2d5649] transition"
+                  >
+                    MY JOBS
+                  </Link>
+                </li>
 
-          <>
-          
-      <li>
-        <Link
-          to="/job/getall"
-          onClick={() => setShow(false)}
-          className="text-[#184235] lg:text-[#f1f3f6] text-md font-semibold lg:font-light hover:text-[#2d5649] transition"
-        >
-          ALL JOBS
-        </Link>
-      </li>
 
-      <li>
-        <Link
-          to="/applications/me"
-          onClick={() => setShow(false)}
-          className="text-[#184235] lg:text-[#f1f3f6] text-md font-semibold lg:font-light hover:text-[#2d5649] transition"
-        >
-            : "MY APPLICATIONS"
-        </Link>
-      </li>
-          
-          </>
-        )
-      }
-      
+                <li>
+                  <Link
+                    to="/applications/me"
+                    onClick={() => setShow(false)}
+                    className="text-[#184235] lg:text-[#f1f3f6] text-md font-semibold lg:font-light hover:text-[#2d5649] transition"
+                  >
+                    APPLICANTS
+                  </Link>
+                </li>
+              </>
 
-     
+            ) : (
 
-      <button
-        onClick={handleLogout}
-        className="px-2 py-1 text-md border border-[#184235] lg:border-[#f1f3f6] text-[#184235] lg:text-[#f1f3f6] hover:bg-[#184235] hover:text-white transition"
-      >
-        LOGOUT
-      </button>
-    </ul>
+              <>
 
-    {/* Hamburger */}
-    <div className="block lg:hidden text-[35px] text-[#f1f3f6] cursor-pointer">
-      <GiHamburgerMenu onClick={() => setShow(!show)} />
-    </div>
-  </div>
-</nav>
+                <li>
+                  <Link
+                    to="/job/getall"
+                    onClick={() => setShow(false)}
+                    className="text-[#184235] lg:text-[#f1f3f6] text-md font-semibold lg:font-light hover:text-[#2d5649] transition"
+                  >
+                    ALL JOBS
+                  </Link>
+                </li>
+
+                <li>
+                  <Link
+                    to="/applications/me"
+                    onClick={() => setShow(false)}
+                    className="text-[#184235] lg:text-[#f1f3f6] text-md font-semibold lg:font-light hover:text-[#2d5649] transition"
+                  >
+                    MY APPLICATIONS
+                  </Link>
+                </li>
+
+              </>
+            )
+          }
+
+
+
+
+          <button
+            onClick={handleLogout}
+            className="px-2 py-1 text-md border border-[#184235] lg:border-[#f1f3f6] text-[#184235] lg:text-[#f1f3f6] hover:bg-[#184235] hover:text-white transition"
+          >
+            LOGOUT
+          </button>
+        </ul>
+
+        {/* Hamburger */}
+        <div className="block lg:hidden text-[35px] text-[#f1f3f6] cursor-pointer">
+          <GiHamburgerMenu onClick={() => setShow(!show)} />
+        </div>
+      </div>
+    </nav>
   );
 };
 
