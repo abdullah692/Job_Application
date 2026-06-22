6a9bb074877930b8c5b7e54b165b69b8cfd13d41
Abdullah Siddiqui <abdullahsidd37@gmail.com>
2026-06-22 17:56:35 +0500

    b

--- src/components/Home/HowItWorks.jsx
+++ src/components/Home/HowItWorks.jsx
@@ -15,7 +15,7 @@ const HowItWorks = () => {
     <>
       <div className="howitworks">
         <div className="container">
-          <h3>How JobZee Works</h3>
+          <h3>How Employhub Works</h3>
           <div className="banner" >
             <div className="card" onClick={()=>handleRoute('/register')}>
               <FaUserPlus />
